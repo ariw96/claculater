c33a0234d0d91f0749430c004863acc8bdaa24d9
ariw96 <ariw96@gmail.com>
2022-05-17 07:38:07 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import {useReducer} from 'react';
 import './App.css';
+const ACTIONS = {
+  ADD_DIGIT:"add-digit",
+  CHOOES_OPERATION:"choose-operation",
+  CLEAR:"clear",
+  CLEAR_ENTRY:"clear-entry",
+  EVALUATE:"evaluate"
+}
+function reducer (state,{type,payload}){
+  switch(t)
+}
 
 function App() {
+  const [{currentOperand,pendingOperand,operator},dispatch] = useReducer(calculatorReducer,{})            
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculater-grid">
+      <div className="output">
+        <div className="previous-operand">{previousOperand}  {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
+      </div>
+      <button className= "span-two">AC</button>
+      <button >DEL</button>
+      <button >÷</button>
+      <button >1</button>
+      <button >2</button>
+      <button >3</button>
+      <button >*</button>
+      <button >4</button>
+      <button >5</button>
+      <button >6</button>
+      <button >+</button>
+      <button >7</button>
+      <button >8</button>
+      <button >9</button>
+      <button >9</button>
+      <button >-</button>
+      <button >.</button>
+      <button >0</button>
+      <button className="span-tow">=</button>
     </div>
   );
 }
